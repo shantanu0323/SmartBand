5235233ab3ddb7a7167a829fbbfc4eea1a44d04c
Shantanu Pramanik <srs.shaan@gmail.com>
2018-11-14 11:29:51 +0530

    Data displayed on Dashboard

--- app/src/main/java/com/shaan/smartband/HomeActivity.java
+++ app/src/main/java/com/shaan/smartband/HomeActivity.java
@@ -1,34 +1,87 @@
 package com.shaan.smartband;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.shaan.smartband.Models.HealthData;
 import com.shaan.smartband.Utilities.DataLoader;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 public class HomeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<HealthData>> {
 
+    private static final String TAG = "HomeActivity";
     private static final int LOADER_ID = 1;
-    private static final String REQUEST_URL = "https://api.thingspeak.com/channels/259686/feeds.json";
+    private static final String REQUEST_URL = "https://api.thingspeak.com/channels/312725/feeds.json";
     private boolean alreadyLoaded = false;
+    private PieChart chartBodyTemp;
+    private Button bRefresh;
+    private TextView tvBodyTemp, tvAtmosTemp, tvPulseRate, tvHeatIndex, tvLastUpdated;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        findViews();
         initiateLoader();
+        bRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initiateLoader();
+            }
+        });
+
+//        drawBodyTempChart();
+    }
+
+    private void drawBodyTempChart() {
+        List<PieEntry> entries = new ArrayList<>();
+
+        entries.add(new PieEntry(800, "Green"));
+        entries.add(new PieEntry(3200, ""));
+
+        PieDataSet set = new PieDataSet(entries, "Election Results");
+        PieData data = new PieData(set);
+        chartBodyTemp.setData(data);
+        chartBodyTemp.invalidate(); // refresh
+    }
+
+    private void findViews() {
+//        chartBodyTemp = findViewById(R.id.chartBodyTemp);
+        tvBodyTemp = findViewById(R.id.tvBodyTemp);
+        tvAtmosTemp = findViewById(R.id.tvAtmosTemp);
+        tvPulseRate = findViewById(R.id.tvPulseRate);
+        tvHeatIndex = findViewById(R.id.tvHeatIndex);
+        tvLastUpdated = findViewById(R.id.tvLastUpdated);
+        bRefresh = findViewById(R.id.bRefresh);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Hold On !!!");
+        progressDialog.setMessage("Just a sec while I fetch the details...");
     }
 
     private void initiateLoader() {
@@ -44,6 +97,7 @@ public class HomeActivity extends AppCompatActivity implements LoaderManager.Loa
 
         // If there is a network connection, fetch data
         if (networkInfo != null && networkInfo.isConnected()) {
+            progressDialog.show();
             // Get a reference to the LoaderManager, in order to interact with loaders.
             LoaderManager loaderManager = getSupportLoaderManager();
 
@@ -56,7 +110,7 @@ public class HomeActivity extends AppCompatActivity implements LoaderManager.Loa
 
         } else {
             // No Connection
-            Toast.makeText(getApplicationContext(), "No internet Connection", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "SMART BAND : No internet Connection", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -68,12 +122,34 @@ public class HomeActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(@NonNull Loader<List<HealthData>> loader, List<HealthData> healthDataList) {
+        progressDialog.dismiss();
+        Snackbar.make(findViewById(R.id.bRefresh), "Data Updated Successfully", Snackbar.LENGTH_LONG).show();
         String message = "";
         for (HealthData healthData :
                 healthDataList) {
             message += healthData.display();
         }
         ((TextView) findViewById(R.id.tvData)).setText(message);
+        HealthData healthData = healthDataList.get(healthDataList.size() - 1);
+        tvBodyTemp.setText(healthData.getBodyTemp() + " °C");
+        tvAtmosTemp.setText(healthData.getAtmosTemp() + " °C");
+        tvPulseRate.setText(healthData.getPulseRate() + " bpm");
+        tvHeatIndex.setText(healthData.getHeatIndex());
+        tvLastUpdated.setText("Last Updated on : " + getDateValue(healthData.getCreatedAt()));
+    }
+
+    private String getDateValue(String dateStr) {
+        try {
+            Date date = new SimpleDateFormat("yyyy-MM-ddhh:mm:ss")
+                    .parse(dateStr.replace('T', ' ').replace('Z', ' '));
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy, hh:mm:ss");
+            sdf.setTimeZone(TimeZone.getTimeZone("IST"));
+            return sdf.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Log.e(TAG, "getDateValue: ERROR PARSING DATE", e);
+            return dateStr + " in GMT";
+        }
     }
 
     @Override
